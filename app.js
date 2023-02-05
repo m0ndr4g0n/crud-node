ac786a2d18b289dc48971d1bf2dcc4ec616e2d34
m0ndr4g0n <leonardo_souzac@hotmail.com>
2023-02-05 04:22:42 -0300

    add input validations to forms

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
 const express = require ('express')
 const handlebars = require ('express-handlebars')
-const bodyparser = require ('body-parser')
 const bodyParser = require('body-parser')
 const app = express()
 const admin = require('./routes/admin')
 const path = require ('path')
-//const mongoose = require ('mongoose')
+const mongoose = require ('mongoose')
+const session  = require ('express-session')
+const flash = require ('connect-flash')
 
 // configurações
+    // session
+    app.use(session({
+        secret: 'learningnode',
+        resave: true,
+        saveUninitialized: true
+    }))
+    app.use(flash())
+
+    // middlewares
+    app.use((req,res,next) =>{
+        res.locals.success_msg = req.flash("success_msg"[0])
+        res.locals.error_msg = req.flash("erro_msg"[0])
+        next()
+    })
     // body parser
     app.use(bodyParser.urlencoded({extended: true}))
     app.use(bodyParser.json())
@@ -15,16 +30,22 @@ const path = require ('path')
     app.engine('handlebars', handlebars.engine({ extname: 'handlebars', defaultLayout: 'main', layoutsDir: __dirname + '/views/layouts/' }))
     app.set('view engine', 'handlebars');
     // mongoose
-
+    mongoose.Promise = global.Promise
+    mongoose.connect('mongodb://127.0.0.1/blogapp').then(() =>{
+        console.log('Conectado ao banco.')
+    }).catch((err) =>{
+        console.log('Falha ao conectar ao banco.' + err)
+    })
     // public
     app.use(express.static(path.join(__dirname,'public')))
-// rotas
-    app.get('/', (req,res) =>{
-        res.send('Home page.')
+
+    // rotas
+        app.get('/', (req,res) =>{
+            res.send('Home page.')
+        })
+        app.use('/admin', admin)
+    // outros
+    const PORT = 8081
+    app.listen(PORT, () =>{
+        console.log("Servidor rodando.")
     })
-    app.use('/admin', admin)
-// outros
-const PORT = 8081
-app.listen(PORT, () =>{
-    console.log("Servidor rodando.")
-})
--- routes/admin.js
+++ routes/admin.js
@@ -1,4 +1,7 @@
 const router = require('express').Router()
+const mongoose = require ('mongoose')
+require('../models/Categoria')
+const Categoria = mongoose.model("categorias")
 
 router.get('/', (req, res) =>{
     res.render("./admin/index")
@@ -14,6 +17,41 @@ router.get('/categorias', (req, res) =>{
 
 router.get('/categorias/add', (req,res) =>{
     res.render('./admin/addcategorias')
+})
+
+router.post('/categorias/nova', (req,res) =>{
+
+    let erros = []
+
+    if(!req.body.nome || typeof req.body.nome == undefined){
+        erros.push({texto: "Nome inválido"})
+    }
+
+    if(!req.body.slug || typeof req.body.slug == undefined){
+        erros.push({texto: "Slug inválido"})
+    }
+
+    if (req.body.nome.length < 2){
+        erros.push({texto: 'O nome da categoria é muito pequeno'})
+    }
+
+    if(erros.length > 0){
+        res.render("./admin/addcategorias", {erros: erros})
+    }
+    else{   
+        const novaCategoria = {
+        nome: req.body.nome,
+        slug: req.body.slug
+    }
+
+    new Categoria(novaCategoria).save().then(() => {
+        req.flash("success_msg", "Categoria criada com sucesso!")
+        res.render('./admin/categorias',({success_msg: req.flash('success_msg')}))
+    }).catch((err) =>{
+        req.flash("error_msg", "Houve um erro ao tentar salvar a categoria, tente novamente.")
+        console.log('Erro ao salvar categoria: ' + err)
+    })}
+
 })
 
 
